ae0d839ee7d68681e287adf40a2a6719cfc5aaed
liyi.54 <adeliyiseyi@gmail.com>
2020-10-09 00:49:12 +0100

    added message and notifications

--- app/__init__.py
+++ app/__init__.py
@@ -30,7 +30,7 @@ def create_app(config_class=Config):
     app.elasticsearch = Elasticsearch(app.config['ELASTICSEARCH_URL']) if app.config['ELASTICSEARCH_URL'] \
         else None
     db.init_app(app)
-    migrate.init_app(app, db)
+    migrate.init_app(app, db, render_as_batch=True)
     login.init_app(app)
     mail.init_app(app)
     bootstrap.init_app(app)
--- app/main/routes.py
+++ app/main/routes.py
@@ -11,6 +11,8 @@ from flask_babel import _, get_locale
 from guess_language import guess_language
 from app.translate import translate
 from flask import jsonify
+from app.models import Message, Notification
+from .forms import MessageForm
 
 
 @bp.before_request
@@ -159,3 +161,44 @@ def popup(username):
     user = User.query.filter_by(username=username).first_or_404()
     form = EmptyForm()
     return render_template('main/user_popup.html', form=form, user=user)
+
+
+@bp.route('/send_message/@<recipient>', methods=['GET', 'POST'])
+@login_required
+def send_message(recipient):
+    user = User.query.filter_by(username=recipient).first_or_404()
+    form = MessageForm()
+    if form.validate_on_submit():
+        message = Message(author=current_user, recipient=user, body=form.message.data)
+        db.session.add(message)
+        user.add_notification('unread_message_count', user.new_messages())
+        db.session.commit()
+        flash(_('Your message has been sent'))
+        return redirect(url_for('main.user', username=recipient))
+    return render_template('main/send_message.html', recipient=recipient, title=_('Send Message'), form=form)
+
+
+@bp.route('/messages')
+@login_required
+def messages():
+    current_user.last_message_read_time = datetime.utcnow()
+    current_user.add_notification('unread_message_count', 0)
+    db.session.commit()
+    page = request.args.get('page', 1, type=int)
+    messages = current_user.messages_received.order_by(Message.timestamp.desc()).paginate(page,
+                                                                                 current_app.config['RESULTS_PER_PAGE'],
+                                                                                 False)
+    next_url = url_for('main.messages', page=messages.next_num) if messages.has_next else None
+    prev_url = url_for('main.messages', page=messages.prev_num) if messages.has_prev else None
+    return render_template('main/messages.html', prev_url=prev_url, next_url=next_url, messages=messages.items)
+
+
+@bp.route('/notifications')
+@login_required
+def notifications():
+    since = request.args.get('since', 0.0, type=float)
+    notifications = current_user.notifications.filter(Notification.timestamp > since).order_by(Notification.timestamp.asc())
+    return jsonify([{'name': n.name,
+                     'data': n.get_data(),
+                     'timestamp': n.timestamp} for n in notifications])
+
